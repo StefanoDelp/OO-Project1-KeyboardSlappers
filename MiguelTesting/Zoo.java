64fb2ae67fe3c72a110a3325a5bd70383a499555
Miguel Vazquez <mvazquez@engr2-2-79-77-dhcp.int.colorado.edu>
2019-09-19 11:56:26 -0600

    seperate zoo file

---
+++ MiguelTesting/Zoo.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Zoo {
+    //Put the list in here instead of in main
+    ArrayList<Animal> Animals = new ArrayList<>();
+    
+    //Adding an animal 
+    public void add(Animal x) {
+        Animals.add(x);
+    }
+}
--- MiguelTesting/main.java
+++ MiguelTesting/main.java
@@ -8,13 +8,3 @@ public class Main {
         zoo.makeNoise();
     }
 }
-
-public class Zoo {
-    //Put the list in here instead of in main
-    ArrayList<Animal> Animals = new ArrayList<>();
-    
-    //Adding an animal 
-    public void add(Animal x) {
-        Animals.add(x);
-    }
-}
