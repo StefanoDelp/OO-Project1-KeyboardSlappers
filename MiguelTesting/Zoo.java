fe87c3c8cf3e7fbd6a592ae6857e9b06ea462362
Miguel Vazquez <mvazquez@rgnt1-45-103-dhcp.int.colorado.edu>
2019-09-19 15:47:54 -0600

    Added wolfs into the zoo

--- MiguelTesting/Zoo.java
+++ MiguelTesting/Zoo.java
@@ -5,11 +5,15 @@ public class Zoo {
     public static void main(String[] args) {
         Zookeeper zoo = new Zookeeper();
         Dog x = new Dog();
-        x.setName("Dummy the Dog");
-        zoo.add(x);
         Animal y = new Dog(); //Poly morphism
-        zoo.add(y);
+        Wolf a = new Wolf();
+        Animal b = new Wolf();
+        a.setName("Willy the Wolf");
+        b.setName("Willard the Wolf");
+        x.setName("Dummy the Dog");
         y.setName("Stupid the Dog");
+        zoo.add(x);
+        zoo.add(y);
         zoo.wakeUp();
         zoo.rollCall();
         zoo.roam();
