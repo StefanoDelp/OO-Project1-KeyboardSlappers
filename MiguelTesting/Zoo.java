b5214cec46e71539fb60f51e7b3f99dcdfaab83b
Miguel Vazquez <mvazquez@engr2-2-79-77-dhcp.int.colorado.edu>
2019-09-19 13:12:51 -0600

    Renaming main to zoo and added zookeeper

--- MiguelTesting/Zoo.java
+++ MiguelTesting/Zoo.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
+
 public class Zoo {
-    //Put the list in here instead of in main
-    ArrayList<Animal> Animals = new ArrayList<>();
-    
-    //Adding an animal 
-    public void add(Animal x) {
-        Animals.add(x);
-    }
-    public void rollCall() {
-        for(Animal var : Animals) {
-            System.out.println(var.makeNoise());
-        }
+    public static void main(String[] args) {
+        Zoo zoo = new Zoo();
+        Dog x = new Dog();
+        x.setName("Dummy the Dog");
+        zoo.add(x);
+        Animal y = new Dog(); //Poly morphism
+        zoo.add(y);
+        y.setName("Stupid the Dog");
+        Zookeeper.rollCall(zoo);
+
     }
 }
---
+++ MiguelTesting/Zookeeper.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Zookeeper {
+    //Put the list in here instead of in main
+    ArrayList<Animal> Animals = new ArrayList<>();
+    
+    //Adding an animal 
+    public void add(Animal x) {
+        Animals.add(x);
+    }
+    public void rollCall() {
+        for(Animal var : Animals) {
+            System.out.println(var.makeNoise());
+        }
+    }
+}
