05a1739e45c8394946a6838ba2e44e983147b192
Miguel Vazquez <mvazquez@engr2-2-79-77-dhcp.int.colorado.edu>
2019-09-19 12:32:23 -0600

    Added an override in canine to see if I can work

--- MiguelTesting/main.java
+++ MiguelTesting/main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         Zoo zoo = new Zoo();
         Dog x = new Dog();
+        x.makeNoise();
         x.setName("Dummy the Dog");
         zoo.add(x);
         zoo.rollCall();
